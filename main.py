5eb25e1211caa733152f8f018c213a2d908378b3
xxcosita3czxx <kokosove18@gmail.com>
2023-08-23 22:32:52 +0200

    removin commented code and clearin code

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ import utils.cosita_toolkit as ctkit
 import coloredlogs
 
 coloredlogs.install(level='INFO', fmt='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-
-# Create a logger
 logger = logging.getLogger(__name__)
-
-
 
 ## vars
 help_list="""**HELP**
--- run.py
+++ run.py
@@ -4,28 +4,10 @@ import threading
 import utils.cosita_toolkit as ctkit
 import logging
 import coloredlogs
-# Configure colorlog with colors
-# handler = colorlog.StreamHandler()
-# handler.setFormatter(colorlog.ColoredFormatter(
-#     '%(log_color)s%(asctime)s %(levelname)s: %(message)s',
-#     datefmt='%Y-%m-%d %H:%M:%S',
-#     log_colors={
-#         'DEBUG': 'cyan',
-#         'INFO': 'green',
-#         'WARNING': 'yellow',
-#         'ERROR': 'red',
-#         'CRITICAL': 'bold_red',
-#     }
-# ))
-
-# # Create a logger
-# logger = logging.getLogger(__name__)
-# logger.addHandler(handler)
-# logger.setLevel(logging.INFO)
 
 coloredlogs.install(level='INFO', fmt='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-# Create a logger
+
 logger = logging.getLogger(__name__)
 
 
