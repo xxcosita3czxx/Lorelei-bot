f68e910f1eb2bd998245157cc67925f3fbae406f
xxcosita3czxx <kokosove18@gmail.com>
2023-08-23 18:02:55 +0200

    update run.py

--- run.py
+++ run.py
@@ -14,7 +14,16 @@ def update_loop():
     while True:
         update_and_run()
         time.sleep(60)
+def main_script_monitor():
+    while True:
+        main_pid = os.popen("pgrep -f main.py").read()
+        if not main_pid:
+            print("main.py is not running. Restarting...")
+            update_and_run()
+        time.sleep(10)
 if __name__ == "__main__":
     update_thread = threading.Thread(target=update_loop)
     update_thread.start()
+    monitor_thread = threading.Thread(target=main_script_monitor)
     os.system("python main.py")
+    monitor_thread.start()
