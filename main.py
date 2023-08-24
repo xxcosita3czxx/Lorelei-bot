f845ce0a090ba7385c005d651f0ab691655feac2
xxcosita3czxx <kokosove18@gmail.com>
2023-08-24 22:27:05 +0200

    DEBUG

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import logging
 import utils.cosita_toolkit as ctkit
 import coloredlogs
 
-coloredlogs.install(level='INFO', fmt='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+coloredlogs.install(level="DEBUG", fmt='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger(__name__)
 
 ## vars
