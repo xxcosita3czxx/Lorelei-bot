235ff016cc93549654e02171a89d3a8574a0eec4
xxcosita3czxx <kokosove18@gmail.com>
2024-04-28 18:51:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -214,7 +214,8 @@ async def slowmode(interaction: discord.Interaction, time: app_commands.Transfor
         await interaction.response.send_message(f"Slowmode has been set to {format_timespan(time)} seconds", ephemeral=True)
         await interaction.channel.send(embed=discord.Embed(description=f"Slow mode has been set to {format_timespan(time)} to {interaction.channel.mention}", color=discord.Color.green()))
 
-@tree.command(name="clear", description="Clear n messages specific user", guild=discord.Object(id=867851000286806016))
+@tree.command(name="clear", description="Clear n messages specific user")
+@app_commands.describe("clear messages")
 @app_commands.default_permissions(manage_messages=True)
 async def self(interaction: discord.Interaction, amount: int, member: discord.Member):
     channel = interaction.channel
