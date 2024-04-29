06b7ceec96a6f35a2bb6938686067649ae2962b3
xxcosita3czxx <kokosove18@gmail.com>
2024-04-29 16:42:21 +0200

    fixing typo

--- config.py
+++ config.py
@@ -1,4 +1,21 @@
 import discord
 ##############################################
 
-status = discord.Status.online
+
+####### Log Level #########
+#
+# Available options
+
+
+######### Status ##########
+#
+# Offline status
+#status = discord.Status.offline
+#
+# Online status
+#status = discord.Status.online
+#
+# Do not Disturb status (default)
+status = discord.Status.dnd
+###########################
+###
