b69d83caea53e70065f217c599f70753b1dbaf44
xxcosita3czxx <kokosove18@gmail.com>
2024-04-29 16:18:06 +0200

    anothr typo

--- run.py
+++ run.py
@@ -20,7 +20,7 @@ def update_and_run():
             logger.warning(f"git pull failed, pulling with http api: {e}")
             update_success = ctkit.github_api.update_repo_files_http("xxcosita3czxx", "lorelei-bot", "main",["run.py","main.py"])
         logger.info(update_success)
-        if update_succes == 2:
+        if update_success == 2:
             os.system("pkill -f main.py")
             logger.info("success, killing main.py...")
         elif update_success == 1:
