ba7b636e1bc5299ce48d5e8e214b0555c4209430
xxcosita3czxx <kokosove18@gmail.com>
2024-04-30 21:52:40 +0200

    fixin

--- main.py
+++ main.py
@@ -336,9 +336,12 @@ async def self(interaction: discord.Interaction, amount: int, member: discord.Me
 
     def check_author(m):
         return None or m.author.id == member.id
-    
-    await channel.purge(limit=amount, check=check_author)
-    await interaction.channel.send(embed=discord.Embed(description=f"Successfully deleted {amount} messages from {m.name}", color=discord.Color.green()))
+    if member == None:
+        await channel.purge(limit=amount)
+        await interaction.channel.send(embed=discord.Embed(description=f"Successfully deleted {amount} messages from {member.name}", color=discord.Color.green()))
+    else:
+        await channel.purge(limit=amount, check=check_author)
+        await interaction.channel.send(embed=discord.Embed(description=f"Successfully deleted {amount} messages from {member.name}", color=discord.Color.green()))
 
 with open(".secret.key", "r") as key:
     token = key.read()
