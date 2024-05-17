d54d2839e4ff15ecb9e892fc0a7a783801161f86
xxcosita3czxx <kokosove18@gmail.com>
2024-05-17 18:20:06 +0200

    fixin ruff rules

--- run.py
+++ run.py
@@ -54,8 +54,8 @@ def update_and_run():
         try:
             ctkit.update_script_from_github("xxcosita3czxx","lorelei-bot","main.py","main.py")
             ctkit.update_script_from_github("xxcosita3czxx","lorelei-bot","run.py","run.py")
-        except Exception:
-            pass
+        except Exception as e:
+            logger.warning(e)
 def update_loop():
     while True:
         update_and_run()
@@ -69,12 +69,12 @@ def Is_Alive():
                 logger.debug("main.py is running at PID: " + str(main_pid))
         if not main_pid:
             logger.info("main.py is not running. Restarting...")
-            os.system("python3 main.py")
+            os.system("python3 main.py")  # noqa: S605, S607
         time.sleep(config.Is_Alive_time)
 def update_cosita_tk():
     while True:
         try:
-            os.system("python3 utils/cosita_toolkit.py")
+            os.system("python3 utils/cosita_toolkit.py")  # noqa: S605, S607
         except Exception:
             logger.error("CosTK update FAILED")
         time.sleep(config.costk_update)
