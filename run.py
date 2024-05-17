2281bbb4af65fd97e794e88e6acb2cfa1d12dd99
xxcosita3czxx <kokosove18@gmail.com>
2024-05-17 17:15:25 +0200

    Ruff Rules

--- run.py
+++ run.py
@@ -1,12 +1,19 @@
-import os
-import time
-import threading
-import utils.cosita_toolkit as ctkit
 import logging
+import os
+import threading
+import time
+
 import coloredlogs
 import psutil
+
 import config
-coloredlogs.install(level=config.loglevel, fmt='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+import utils.cosita_toolkit as ctkit
+
+coloredlogs.install(
+    level=config.loglevel,
+    fmt='%(asctime)s %(levelname)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 
 logger = logging.getLogger(__name__)
 # Get the full path to the directory of this script
@@ -19,14 +26,14 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 #                update_success = ctkit.github_api.pull_repo(".")
 #            except Exception as e:
 #                logger.warning(f"git pull failed, pulling with http api: {e}")
-#                update_success = ctkit.github_api.update_repo_files_http("xxcosita3czxx", "lorelei-bot", "main",["run.py","main.py"])
+#                update_success = ctkit.github_api.update_repo_files_http("xxcosita3czxx", "lorelei-bot", "main",["run.py","main.py"])  # noqa: E501
 #            logger.info(update_success)
 #            if update_success == 2:
 #                os.system("pkill -f main.py")
 #                logger.info("success, killing main.py...")
 #                main_pid = None
 #                for process in psutil.process_iter(['pid', 'name', 'cmdline']):
-#                    if 'python3' in process.info['name'] and 'main.py' in ' '.join(process.info['cmdline']):
+#                    if 'python3' in process.info['name'] and 'main.py' in ' '.join(process.info['cmdline']):  # noqa: E501
 #                        main_pid = process.info['pid']
 #                        logger.debug("main.py is running at PID: " + str(main_pid))
 #                if not main_pid:
@@ -37,16 +44,18 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 #            else:
 #                logger.error("UPDATE FAILED")
 #        except Exception as e:
-#            logger.error(f"Error while trying to update, Install git, or if issue persist after autoscheduled update, create issue page on github -->> {e}")
+#            logger.error(f"Error while trying to update, Install git, or if issue persist after autoscheduled update, create issue page on github -->> {e}")  # noqa: E501
 #    else:
 #        logger.warning("UPDATER IS OFF, YOU WILL HAVE TO UPDATE MANUALLY")
 
 
 def update_and_run():
-    if config.autoupdate == True:
+    if config.autoupdate:
         try:
             ctkit.update_script_from_github("xxcosita3czxx","lorelei-bot","main.py","main.py")
             ctkit.update_script_from_github("xxcosita3czxx","lorelei-bot","run.py","run.py")
+        except Exception:
+            pass
 def update_loop():
     while True:
         update_and_run()
@@ -55,7 +64,7 @@ def Is_Alive():
     while True:
         main_pid = None
         for process in psutil.process_iter(['pid', 'name', 'cmdline']):
-            if 'python3' in process.info['name'] and 'main.py' in ' '.join(process.info['cmdline']):
+            if 'python3' in process.info['name'] and 'main.py' in ' '.join(process.info['cmdline']):  # noqa: E501
                 main_pid = process.info['pid']
                 logger.debug("main.py is running at PID: " + str(main_pid))
         if not main_pid:
@@ -65,8 +74,8 @@ def Is_Alive():
 def update_cosita_tk():
     while True:
         try:
-            os.system(f"python3 utils/cosita_toolkit.py")
-        except Exception as e:
+            os.system("python3 utils/cosita_toolkit.py")
+        except Exception:
             logger.error("CosTK update FAILED")
         time.sleep(config.costk_update)
 if __name__ == "__main__":
