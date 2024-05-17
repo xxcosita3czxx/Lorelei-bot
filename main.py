686dddb9ffa9cf690419aa86e45d48112068ec06
xxcosita3czxx <kokosove18@gmail.com>
2024-05-17 21:38:05 +0200

    maybe?

--- main.py
+++ main.py
@@ -453,12 +453,12 @@ async def ban(interaction: discord.Interaction, member: discord.Member, reason: 
         ephemeral=False,
     )
 
+configure = app_commands.Group(name="configure",description="Config for server")
 
-@app_commands.Group(name="configure",description="Config for server")
-def configure():
-    @tree.command(name="test2",description="just test for subcommands")
-    def test2(interaction: discord.Interaction):
-        interaction.response.send_message(content="test2",ephemeral=True)
+@configure.command(name="test2",description="just test for subcommands")
+def test2(interaction: discord.Interaction):
+    interaction.response.send_message(content="test2",ephemeral=True)
+
 
 @tree.command(name="unban", description="Unban a user")
 @app_commands.describe(member="User to unban", reason="Reason for unban")
