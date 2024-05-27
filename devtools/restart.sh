2a5fbd40e727cd32e13e0d202affd312effcb4db
xxcosita3czxx <kokosove18@gmail.com>
2024-05-27 18:56:05 +0200

    persistent config restart devtool

--- devtools/restart.sh
+++ devtools/restart.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+cd "$SCRIPT_DIR/.." || exit
+
+cp config.py devtools/config.py.bak
 sudo git pull
+cp devtools/config.py.bak config.py
 sudo systemctl restart lorelei.service
 journalctl -xfu lorelei.service
