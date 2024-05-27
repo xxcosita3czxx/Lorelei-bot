328f2c038c84d01a7f481d6d6ff22330d1feb262
xxcosita3czxx <kokosove18@gmail.com>
2024-05-27 18:58:57 +0200

    sudoing

--- devtools/restart.sh
+++ devtools/restart.sh
@@ -3,9 +3,9 @@
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 cd "$SCRIPT_DIR/.." || exit
 
-cp config.py devtools/config.py.bak
-git stash
+sudo cp config.py devtools/config.py.bak
+sudo git stash
 sudo git pull
-cp devtools/config.py.bak config.py
+sudo cp devtools/config.py.bak config.py
 sudo systemctl restart lorelei.service
 journalctl -xfu lorelei.service
