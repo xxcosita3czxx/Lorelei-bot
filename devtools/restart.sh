417f56daf7850ee35ec57185d82ed1d847f25f0a
xxcosita3czxx <kokosove18@gmail.com>
2024-05-27 18:56:59 +0200

    forgor stashing

--- devtools/restart.sh
+++ devtools/restart.sh
@@ -4,6 +4,7 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 cd "$SCRIPT_DIR/.." || exit
 
 cp config.py devtools/config.py.bak
+git stash
 sudo git pull
 cp devtools/config.py.bak config.py
 sudo systemctl restart lorelei.service
