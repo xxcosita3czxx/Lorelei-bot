2a1ba4ffa3b23928dfebb622146283b44e2e7f02
xxcosita3czxx <kokosove18@gmail.com>
2024-05-31 18:34:34 +0200

    restart saves config

--- devtools/restart.sh
+++ devtools/restart.sh
@@ -3,9 +3,11 @@
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 cd "$SCRIPT_DIR/.." || exit
 sudo pip install -r requirements.txt --break
-sudo cp config.py devtools/config.py.bak
+sudo cp config.py ../config.py.bak
+sudo cp -r data ../data.bak/
 sudo git stash
 sudo git pull
-sudo cp devtools/config.py.bak config.py
+sudo mv ../config.py.bak config.py
+sudo mv ../data.bak ./data
 sudo systemctl restart lorelei.service
 journalctl -xfu lorelei.service
