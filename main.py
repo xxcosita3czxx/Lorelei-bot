752f09040cdab324fb49ef57b07f945e7651d6e8
xxcosita3czxx <kokosove18@gmail.com>
2024-06-03 20:53:42 +0200

    putting em individually

--- main.py
+++ main.py
@@ -1184,12 +1184,6 @@ class reviews(discord.ui.View):
         )
         return review_embed
 
-    def resp_embed(self,interaction:discord.Interaction):
-        response_embed = discord.Embed(
-            title=lang.get(uconfig.get(interaction.user.id,"Appearance","language"),"TicketingCommand","embed_review_resp_title"),
-        )
-        return response_embed
-
     async def disable_all_buttons(self, interaction: discord.Interaction):
         for child in self.children:
             if isinstance(child, discord.ui.Button):
@@ -1199,32 +1193,47 @@ class reviews(discord.ui.View):
     @discord.ui.button(label="1 star")
     async def rev_star1(self, interaction: discord.Interaction, button: discord.Button):  # noqa: E501
         await self.disable_all_buttons(interaction)
+        response_embed = discord.Embed(
+            title=lang.get(uconfig.get(interaction.user.id,"Appearance","language"),"TicketingCommand","embed_review_resp_title"),
+        )
         await interaction.user.send(
-            content=self.resp_embed(interaction),
+            content=response_embed,
         )
     @discord.ui.button(label="2 star")
     async def rev_star2(self, interaction: discord.Interaction, button: discord.Button):  # noqa: E501
         await self.disable_all_buttons(interaction)
+        response_embed = discord.Embed(
+            title=lang.get(uconfig.get(interaction.user.id,"Appearance","language"),"TicketingCommand","embed_review_resp_title"),
+        )
         await interaction.user.send(
-            content=self.resp_embed(interaction),
+            content=response_embed,
         )
     @discord.ui.button(label="3 star")
     async def rev_star3(self, interaction: discord.Interaction, button: discord.Button):  # noqa: E501
         await self.disable_all_buttons(interaction)
+        response_embed = discord.Embed(
+            title=lang.get(uconfig.get(interaction.user.id,"Appearance","language"),"TicketingCommand","embed_review_resp_title"),
+        )
         await interaction.user.send(
-            content=self.resp_embed(interaction),
+            content=response_embed,
         )
     @discord.ui.button(label="4 star")
     async def rev_star4(self, interaction: discord.Interaction, button: discord.Button):  # noqa: E501
         await self.disable_all_buttons(interaction)
+        response_embed = discord.Embed(
+            title=lang.get(uconfig.get(interaction.user.id,"Appearance","language"),"TicketingCommand","embed_review_resp_title"),
+        )
         await interaction.user.send(
-            content=self.resp_embed(interaction),
+            content=response_embed,
         )
     @discord.ui.button(label="5 star")
     async def rev_star5(self, interaction: discord.Interaction, button: discord.Button):  # noqa: E501
         await self.disable_all_buttons(interaction)
+        response_embed = discord.Embed(
+            title=lang.get(uconfig.get(interaction.user.id,"Appearance","language"),"TicketingCommand","embed_review_resp_title"),
+        )
         await interaction.user.send(
-            content=self.resp_embed(interaction),
+            content=response_embed,
         )
 class confirm(discord.ui.View):
 
