d6b371168d543714ec268bdf8c3428b62b8ed5d1
xxcosita3czxx <kokosove18@gmail.com>
2024-08-15 15:57:49 +0200

    helper fix response

--- helper.py
+++ helper.py
@@ -2,8 +2,15 @@ import logging
 import socket
 import sys
 
-logger = logging.getLogger(name=__name__)
-logger.setLevel("DEBUG")
+import coloredlogs
+
+import config
+
+coloredlogs.install(
+    level=config.loglevel,
+    fmt='%(asctime)s %(levelname)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
 
 def send_command(command):
     try:
@@ -11,14 +18,14 @@ def send_command(command):
             client.connect(('localhost', 9920))  # Connect to the bot's socket
             client.sendall(command.encode('utf-8'))
             response = client.recv(1024).decode('utf-8')
-            logger.info(response)
+            logging.info(response)
 
     except ConnectionRefusedError:
-        logger.error("Bot is not running")
+        logging.error("Bot is not running")
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         command = ' '.join(sys.argv[1:])
         send_command(command)
     else:
-        logger.error("Please provide a command to send.")
+        logging.error("Please provide a command to send.")
