2c7bdddb6be29a7d0aee9aff4faf57abda71c1d1
xxcosita3czxx <kokosove18@gmail.com>
2024-08-16 12:13:05 +0200

    bugreport fix so it doesnt interfere with echo

--- commands/other/bugreport.py
+++ commands/other/bugreport.py
@@ -10,7 +10,7 @@ import config
 class BugReport(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    @app_commands.command(name="echo",description="Echoes message in embed")
+    @app_commands.command(name="bugreport",description="Here you can report bug")
     @app_commands.checks.cooldown(1, 45, key=lambda i: (i.guild_id, i.user.id))
     async def bugreport(self,interaction: discord.Interaction,channel:discord.channel.TextChannel, title:str="", text:str=""):  # noqa: E501
         if config.bugreport:
@@ -19,6 +19,8 @@ class BugReport(commands.Cog):
                     pass
             except Exception as e:
                 interaction.response.send_message(f"There was error while making bugreport. Please report on Support server or github. \nError: {e}",ephemeral=True)  # noqa: E501
+        else:
+            interaction.response.send_message("Bug reports disabled in config.", ephemeral=True)  # noqa: E501
 
 async def setup(bot:commands.Bot):
     cog = BugReport(bot=bot)
