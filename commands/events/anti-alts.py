1ece51cb5c5155be427e5371e7cc55d886e95d96
xxcosita3czxx <kokosove18@gmail.com>
2024-08-23 07:22:47 +0200

    ruff checks

--- commands/events/anti-alts.py
+++ commands/events/anti-alts.py
@@ -16,10 +16,10 @@ class AntiAlts(commands.Cog):
     async def anti_alts(self, member:discord.Member):
         if gconfig.get(id=member.guild.id,title="SECURITY",key="antialts-enabled"):  # noqa: SIM102
             if member.created_at < gconfig.get(id=member.guild.id,title="SECURITY",key="antialts-time"):  # noqa: E501
-                text = "Your account was detected to be an Alternative account, please join with your main account or wait for {time} until joining again."
+                text = "Your account was detected to be an Alternative account, please join with your main account or wait for {time} until joining again."  # noqa: E501
                 embed = discord.Embed(
                     title="ALT Account Detected!",
-                    description=text
+                    description=text,
                 )
                 member.send(embed=embed)
                 member.kick("Alternative Account [Lorelei]")
