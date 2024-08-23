7ef01662b8c575bd44997212b60576461594dd67
xxcosita3czxx <kokosove18@gmail.com>
2024-08-23 07:20:03 +0200

    just some logging + making antialts main function

--- commands/events/anti-alts.py
+++ commands/events/anti-alts.py
@@ -2,20 +2,29 @@ import logging
 
 import discord
 from discord.ext import commands
-from humanfriendly import format_timespan
 
 from utils.configmanager import gconfig
 
+#from humanfriendly import format_timespan
 
 class AntiAlts(commands.Cog):
     def __init__(self, bot):
+        super().__init__()
         self.bot = bot
 
     @commands.Cog.listener("on_member_join")
     async def anti_alts(self, member:discord.Member):
         if gconfig.get(id=member.guild.id,title="SECURITY",key="antialts-enabled"):  # noqa: SIM102
             if member.created_at < gconfig.get(id=member.guild.id,title="SECURITY",key="antialts-time"):  # noqa: E501
-                logging.warning("Account under time specified")
-
+                text = "Your account was detected to be an Alternative account, please join with your main account or wait for {time} until joining again."
+                embed = discord.Embed(
+                    title="ALT Account Detected!",
+                    description=text
+                )
+                member.send(embed=embed)
+                member.kick("Alternative Account [Lorelei]")
+            else:
+                logging.debug("Acc okay")
+        logging.debug("antialts disabled :<")
 async def setup(bot:commands.Bot):
     await bot.add_cog(AntiAlts(bot))
