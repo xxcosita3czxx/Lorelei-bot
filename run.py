e82596044857622d5a208abfa2a5a5f8ab206b15
xxcosita3czxx <kokosove18@gmail.com>
2025-02-23 17:10:38 +0100

    level for supporting pterodactyl

--- run.py
+++ run.py
@@ -56,7 +56,9 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 def update():
     if config.autoupdate:
         try:
+            os.system("git stash push -- config.py") # noqa: S605
             os.system("git pull") # noqa: S605
+            os.system("git stash pop") # noqa: S605
         except Exception as e:
             logger.warning("UPDATER FAILED")
             logger.warning(e)
@@ -86,6 +88,11 @@ def update_cosita_tk():
             logger.error(lang.get(conflang,"RunnerLogs","err_costk_update_fail"))
         time.sleep(config.costk_update)
 
+def ptero_mode():
+        while True:
+            for line in sys.stdin():
+                os.system("python3 helper.py " + line)  # noqa: S605
+
 @click.command()
 @click.option("--update",is_flag=True, help="Updates the bot and exits")
 def main(update):
@@ -94,13 +101,12 @@ def main(update):
         os.system("python3 utils/cosita_toolkit.py") # noqa: S605
         sys.exit()
 
-    if not os.path.exists(".secret.key") or open(".secret.key").read().strip() == "":
+    if not os.path.exists(".secret.key") or open(".secret.key").read().strip() == "":  # noqa: E501, SIM115
         logger.error("TOKEN NOT FOUND, PLEASE ADD TOKEN TO .secret.key")
         if not os.path.exists(".secret.key"):
             with open(".secret.key", "w") as f:
                 f.write("")
         sys.exit()
-
     monitor_thread = threading.Thread(target=Is_Alive)
     update_thread = threading.Thread(target=update_loop)
     update_costk_thread = threading.Thread(target=update_cosita_tk)
@@ -109,6 +115,8 @@ def main(update):
     update_thread.start()
     update_costk_thread.start()
 
+    if os.getenv("P_SERVER_UUID") is not None:
+        ptero_mode()
 
 if __name__ == "__main__":
     main()
