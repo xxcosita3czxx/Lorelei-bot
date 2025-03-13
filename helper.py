344b1f2df3a6133b9f99e27bf5bcfc68ed8d72c2
xxcosita3czxx <kokosove18@gmail.com>
2025-03-13 10:28:05 +0100

    update of helper

--- helper.py
+++ helper.py
@@ -17,7 +17,7 @@ def send_command(command:str):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
             client.connect(('localhost', 9920))  # Connect to the bot's socket
             client.sendall(command.encode('utf-8'))
-            if command.startswith("reload_all"):
+            if command.startswith("extensions reload_all"):
                 logging.info("Please wait, this may take a minute or two...")
             response = client.recv(1024).decode('utf-8')
             logging.info(response)
