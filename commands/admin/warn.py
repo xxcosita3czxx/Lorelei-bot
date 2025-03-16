be227636538b8bcf0387a1d9e9635dd8dd430b82
xxcosita3czxx <kokosove18@gmail.com>
2025-03-16 23:39:02 +0100

    basic warn system

--- commands/admin/warn.py
+++ commands/admin/warn.py
@@ -2,7 +2,8 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-#TODO Warn system saving to server config in usable way
+from utils.configmanager import gconfig
+
 #TODO Automatic bans and timeouts on warns
 #TODO Add automations to configs
 
@@ -10,12 +11,19 @@ class Warn(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command(name="warn", description="Warns user")
-    async def warn(self,interaction:discord.Interaction):
-        await interaction.response.send_message("You got warned lol")
+    @app_commands.command(name="warn", description="Warns a user.")
+    async def warn(self, interaction: discord.Interaction, user: discord.Member, reason: str):  # noqa: E501
+        guild_id = interaction.guild.id
+        user_id = user.id
 
+        # Add or update the user's warn count
+        if gconfig.get(guild_id, "warns", user_id,default=None) is not None:
+            gconfig.set(guild_id, "warns", user_id, gconfig.get(guild_id, "warns", user_id) + 1)  # noqa: E501
+        else:
+            gconfig.set(guild_id, "warns", user_id, 1)
+
+        await interaction.response.send_message(f"{user.mention} has been warned for: {reason}. They now have {gconfig.get(guild_id,"warns",user_id)} warns.")  # noqa: E501
 
 async def setup(bot:commands.Bot):
-#    cog = Warn(bot)
-#    await bot.add_cog(cog)
-    pass
+    cog = Warn(bot)
+    await bot.add_cog(cog)
