7fe76cd2d101d5870a85dc47926ff373817e836f
xxcosita3czxx <kokosove18@gmail.com>
2025-03-16 16:52:35 +0100

    help page instead of adding command

--- commands/other/bugreport.py
+++ commands/other/bugreport.py
@@ -59,5 +59,5 @@ class BugReport(commands.Cog):
 async def setup(bot:commands.Bot):
     cog = BugReport(bot=bot)
     helpmanager = HelpManager()
-    helpmanager.add_command_to_group("other", "bugreport", "Here you can report bug")  # noqa: E501
+    helpmanager.add_help_page("other", "bugreport", "Here you can report bug")  # noqa: E501
     await bot.add_cog(cog)
