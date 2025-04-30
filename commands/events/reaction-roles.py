ddf1d1884002e44df3c03f36401bc74ccfb1b17d
xxcosita3czxx <kokosove18@gmail.com>
2025-04-30 21:20:30 +0200

    hope this fixes the reaction not adding

--- commands/events/reaction-roles.py
+++ commands/events/reaction-roles.py
@@ -1,9 +1,23 @@
 import discord  # noqa: F401
-from discord import app_commands  # noqa: F401
+import emoji
+from discord import (
+    PartialEmoji,
+    app_commands,  # noqa: F401
+)
 from discord.ext import commands
 
 from utils.configmanager import gconfig
 
+
+def string2emoji(emoji_string:str):
+    if emoji_string.startswith("<:") and emoji_string.endswith(">"):  # Custom emoji
+        # Extract name and ID from the custom emoji string
+        parts = emoji_string.strip("<:>").split(":")
+        name = parts[0]
+        emoji_id = int(parts[1])
+        return PartialEmoji(name=name, id=emoji_id)
+    else:  # Standard Unicode emoji
+        return emoji.emojize(emoji_string, use_aliases=True)
 
 #TODO add reaction roles
 #TODO pls its really importante
@@ -20,7 +34,7 @@ class ReactionRoles(commands.Cog):
         )
         message = await interaction.channel.send(embed=embed)
         gconfig.set(interaction.guild.id,"reaction-roles","message-id",message.id)
-        await message.add_reaction(":thumbs_up:")
+        await message.add_reaction(string2emoji(":thumbs_up:"))
     @commands.Cog.listener("on_reaction_add")
     async def on_react(self, reaction:discord.Reaction, user):
         if user.bot:
