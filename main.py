ef3ba9c38be4b47275c5439f015bdd1c9dd50472
xxcosita3czxx <kokosove18@gmail.com>
2025-04-30 10:36:10 +0200

    tryna specify that it doesnt end with .pyc

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ async def load_cogs(directory, bot):
     for root, _, files in os.walk(directory):
         logger.debug(files)
         for file in files:
-            if file.endswith('.py') and file != '__init__.py':
+            if file.endswith('.py') and file != '__init__.py' and not file.endswith(".pyc"):  # noqa: E501
                 logger.debug(file)
                 # Construct the module path relative to the project root
                 cog_path = os.path.relpath(os.path.join(root, file), start=os.getcwd())  # noqa: E501
