c749a97173396e20f15ddde0c2a83a4066bd7129
xxcosita3czxx <kokosove18@gmail.com>
2025-05-01 18:38:16 +0200

    making multiroles

--- commands/events/reaction-roles.py
+++ commands/events/reaction-roles.py
@@ -9,7 +9,7 @@ from discord.ext import commands
 from utils.configmanager import gconfig
 
 
-def string2emoji(emoji_string:str):
+def string2emoji(emoji_string: str):
     if emoji_string.startswith("<:") and emoji_string.endswith(">"):  # Custom emoji
         # Extract name and ID from the custom emoji string
         parts = emoji_string.strip("<:>").split(":")
@@ -19,6 +19,12 @@ def string2emoji(emoji_string:str):
     else:  # Standard Unicode emoji
         return emoji.emojize(emoji_string)
 
+def emoji2string(emoji_obj: PartialEmoji | str):
+    if isinstance(emoji_obj, PartialEmoji):  # Custom emoji
+        return f"<:{emoji_obj.name}:{emoji_obj.id}>"
+    else:  # Standard Unicode emoji
+        return emoji.demojize(emoji_obj)
+
 #TODO add reaction roles
 #TODO pls its really importante
 class ReactionRoles(commands.Cog):
@@ -27,20 +33,30 @@ class ReactionRoles(commands.Cog):
     # [reaction-roles]
     # <message-id>-<emoji>-role = <role>
     # PEAK EFFICIENCI :fire:
-    @app_commands.command(name="reaction-roles",description="pls dont use, its testing only")  # noqa: E501
+
+    @app_commands.command(name="add-reaction-role",description="pls dont use, its testing only")  # noqa: E501
     @app_commands.default_permissions(administrator=True)  # noqa: E501
-    async def create_reaction(self, interaction: discord.Interaction,title:str,description:str,emoji:str,channel:discord.TextChannel=None):  # noqa: E501
+    async def create_reaction(self, interaction: discord.Interaction,title:str,description:str,emoji:str,messageid:str=None,channel:discord.TextChannel=None):  # noqa: E501
         if channel is None:
             channel = interaction.channel
-        embed = discord.Embed(
-            title=title,
-            description=description,
-            color=discord.Color.blurple(),
-        )
-        message = await channel.send(embed=embed)
-        gconfig.set(interaction.guild.id,"reaction-roles","message-id",message.id)
-        await message.add_reaction(string2emoji(emoji))
-        await interaction.response.send_message("Reaction roles message sent!", ephemeral=True)  # noqa: E501
+        if messageid is None:
+            embed = discord.Embed(
+                title=title,
+                description=description,
+                color=discord.Color.blurple(),
+            )
+            message = await channel.send(embed=embed)
+            gconfig.set(interaction.guild.id,"reaction-roles",f"{message.id}-{emoji2string(emoji)}-role",message.id)
+            await message.add_reaction(string2emoji(emoji))
+            await interaction.response.send_message("Reaction roles message sent!", ephemeral=True)  # noqa: E501
+        else:
+            message = await channel.fetch_message(messageid)
+            if message is None:
+                await interaction.response.send_message("Message not found!", ephemeral=True)  # noqa: E501
+                return
+            gconfig.set(interaction.guild.id,"reaction-roles",f"{message.id}-{emoji2string(emoji)}-role",message.id)
+            await message.add_reaction(string2emoji(emoji))
+            await interaction.response.send_message("Reaction role added!", ephemeral=True)  # noqa: E501
 
     @commands.Cog.listener("on_reaction_add")
     async def on_react(self, reaction:discord.Reaction, user):
@@ -49,20 +65,28 @@ class ReactionRoles(commands.Cog):
 
         # Example: Reaction role logic
         guild = reaction.message.guild
-        if reaction.message.id == gconfig.get(guild.id,"reaction-roles","message-id"): # noqa: E501, SIM102
-            if str(reaction.emoji) == string2emoji(":thumbs_up:"):
-                await user.send("You have been given the role!")
+        reaction_roles_config = gconfig.config.get(str(guild.id), {}).get("reaction-roles", {})  # noqa: E501
+        for key, _ in reaction_roles_config.items():
+            if key.startswith(f"{reaction.message.id}-"):
+                _, emoji, role_id = key.split("-")
+            if str(reaction.emoji) == string2emoji(emoji):
+                role = guild.get_role(int(role_id))
+                if role:
+                    await user.add_roles(role)
 
     @commands.Cog.listener("on_reaction_remove")
     async def on_react_remove(self, reaction, user):
         if user.bot:
             return
-
-        # Example: Reaction role removal logic
         guild = reaction.message.guild
-        if reaction.message.id == gconfig.get(guild.id,"reaction-roles","message-id"): # noqa: E501, SIM102
-            if str(reaction.emoji) == string2emoji(":thumbs_up:"):
-                await user.send("The role has been removed.")
+        reaction_roles_config = gconfig.config.get(str(guild.id), {}).get("reaction-roles", {})  # noqa: E501
+        for key, _ in reaction_roles_config.items():
+            if key.startswith(f"{reaction.message.id}-"):
+                _, emoji, role_id = key.split("-")
+            if str(reaction.emoji) == string2emoji(emoji):
+                role = guild.get_role(int(role_id))
+                if role:
+                    await user.add_roles(role)
 async def setup(bot:commands.Bot):
     cog = ReactionRoles(bot)
     await bot.add_cog(cog)
