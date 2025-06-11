d016aa0db466fa20a3ec3fad2b8ec0a315b85d9e
xxcosita3czxx <kokosove18@gmail.com>
2025-06-11 12:31:48 +0200

    write line

--- devtools/helper_tui.py
+++ devtools/helper_tui.py
@@ -78,7 +78,7 @@ class JournalTUI(App):
         if journal.lines != lines:
             journal.clear()
             for line in lines:
-                journal.write(line)
+                journal.write_line(line)
             journal.scroll_end(animate=False)
 
     async def on_input_submitted(self, event):
