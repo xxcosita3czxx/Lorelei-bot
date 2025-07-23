c2cd0d2eb7233cac234fc1a69aaf637b8f0db64a
xxcosita3czxx <kokosove18@gmail.com>
2025-07-23 14:17:54 +0200

    commit should be only 7 letters

--- commands/fun/counting.py
+++ commands/fun/counting.py
@@ -3,6 +3,9 @@
 #TODO Basic math would be fun
 #TODO Maybe could implement in the level system
 #TODO Be able to set the counting channel
+
+#TODO WARNING, DOESNT WORK
+
 import ast
 import logging
 
