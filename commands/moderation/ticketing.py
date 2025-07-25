8755e419481661969d88b3cb691972cecbad5071
xxcosita3czxx <kokosove18@gmail.com>
2025-07-25 13:48:56 +0200

    updates for future warn system

--- commands/moderation/ticketing.py
+++ commands/moderation/ticketing.py
@@ -9,7 +9,6 @@ from utils.configmanager import gconfig, lang, uconfig
 from utils.guildconfig import GuildConfig
 
 #TODO Claiming
-#TODO Please add embed for reviews
 
 class Ticketing(commands.Cog):
     def __init__(self, bot):
--- commands/moderation/warn.py
+++ commands/moderation/warn.py
@@ -21,12 +21,12 @@ async def add_warns(guild_id, user:discord.Member,interaction:discord.Interactio
         gconfig.set(guild_id, "warns", user_id, gconfig.get(guild_id, "warns", user_id) + 1)  # noqa: E501
     else:
         gconfig.set(guild_id, "warns", user_id, 1)
-    if gconfig.get(guild_id, "warns", user_id) >= gconfig.get(guild_id,"warns-settings","timeout",3):  # type: ignore # noqa: E501
-        user.timeout(reason=reason, until=gconfig.get(guild_id,"warns-settings","timeout_duration",timedelta(hours=5)))  # type: ignore # noqa: E501
+    if gconfig.get(guild_id, "warns", user_id) >= gconfig.get(guild_id,"warns-settings","ban",10):  # noqa: E501
+        await ban_member(user, reason, interaction) # type: ignore
     elif gconfig.get(guild_id, "warns", user_id) >= gconfig.get(guild_id,"warns-settings","kick",5):  # noqa: E501, SIM114
         await kick_member(user, reason, interaction)
-    elif gconfig.get(guild_id, "warns", user_id) >= gconfig.get(guild_id,"warns-settings","ban",10):  # noqa: E501
-        await ban_member(user, reason, interaction) # type: ignore
+    elif gconfig.get(guild_id, "warns", user_id) >= gconfig.get(guild_id,"warns-settings","timeout",3):  # type: ignore # noqa: E501
+        user.timeout(reason=reason, until=gconfig.get(guild_id,"warns-settings","timeout_duration",timedelta(hours=5)))  # type: ignore # noqa: E501
 
 
 class Warn(commands.Cog):
@@ -36,20 +36,28 @@ class Warn(commands.Cog):
     @app_commands.command(name="warn", description="Warns a user.")
     @app_commands.default_permissions(moderate_members=True)
     async def warn(self, interaction: discord.Interaction, user: discord.Member, reason: str):  # noqa: E501
-        guild_id = interaction.guild.id # type: ignore
-        user_id = user.id
+        await add_warns(interaction.guild.id, user,interaction)
+        await interaction.response.send_message(f"{user.mention} has been warned for: {reason}. They now have {gconfig.get(interaction.guild.id,'warns',user.id)} warns.")  # noqa: E501
 
-        await add_warns(guild_id, user,interaction)
+    @app_commands.command(name="unwarn", description="Removes a warn from a user.")
+    @app_commands.default_permissions(moderate_members=True)
+    async def unwarn(self, interaction: discord.Interaction, user: discord.Member):  # noqa: E501
+        if gconfig.get(interaction.guild.id, "warns", user.id, default=0) > 0:
+            gconfig.set(interaction.guild.id, "warns", user.id, gconfig.get(interaction.guild.id, "warns", user.id) - 1)  # noqa: E501
+            await interaction.response.send_message(f"{user.mention} has had a warn removed. Now they have {gconfig.get(interaction.guild.id, 'warns', user.id)} warns.", ephemeral=True)  # noqa: E501
 
-        await interaction.response.send_message(f"{user.mention} has been warned for: {reason}. They now have {gconfig.get(guild_id,'warns',user_id)} warns.")  # noqa: E501
+    @app_commands.command(name="clear-warns", description="Clears all warns for a user.")  # noqa: E501
+    @app_commands.default_permissions(moderate_members=True)
+    async def clear_warns(self, interaction: discord.Interaction, user: discord.Member):  # noqa: E501
+        gconfig.set(interaction.guild.id, "warns", user.id, 0)  # type: ignore
+        await interaction.response.send_message(f"{user.mention} has had their warns cleared.", ephemeral=True)  # noqa: E501
+
     @app_commands.command(name="warns", description="Shows the number of warns a user has.")  # noqa: E501
     @app_commands.default_permissions(moderate_members=True)
     async def warns(self, interaction: discord.Interaction, user: discord.Member):  # noqa: E501
-        guild_id = interaction.guild.id # type: ignore
-        user_id = user.id
-
-        warns = gconfig.get(guild_id, "warns", user_id, default=0)  # type: ignore
+        warns = gconfig.get(interaction.guild.id, "warns", user.id, default=0)  # type: ignore
         await interaction.response.send_message(f"{user.mention} has {warns} warns.", ephemeral=True)  # noqa: E501
+
 async def setup(bot:commands.Bot):
 #    cog = Warn(bot)
 #    await bot.add_cog(cog)
@@ -58,4 +66,13 @@ async def setup(bot:commands.Bot):
     @app_commands.default_permissions(kick_members=True)
     async def warn_context(interaction:discord.Interaction,member:discord.Member):
         add_warns(interaction.guild.id, member.id,interaction) # type: ignore
+
+    @app_commands.context_menu(name="Unwarn")
+    @app_commands.default_permissions(kick_members=True)
+    async def unwarn_context(interaction:discord.Interaction,member:discord.Member):
+        if gconfig.get(interaction.guild.id, "warns", member.id, default=0) > 0:
+            gconfig.set(interaction.guild.id, "warns", member.id, gconfig.get(interaction.guild.id, "warns", member.id) - 1)  # noqa: E501
+            await interaction.response.send_message(f"{member.mention} has had a warn removed. Now they have {gconfig.get(interaction.guild.id, 'warns', member.id)} warns.", ephemeral=True)  # noqa: E501
+
+    #bot.tree.add_command(unwarn_context)
     #bot.tree.add_command(warn_context)
