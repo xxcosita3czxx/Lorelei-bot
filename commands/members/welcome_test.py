79582f1de0bda8fd059e105edcefe58b85648db7
xxcosita3czxx <kokosove18@gmail.com>
2025-09-07 20:35:51 +0200

    time to make welcome messages better

---
+++ commands/members/welcome_test.py
@@ -0,0 +1,17 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+
+class Hello(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name="welcome_embed_test", description="Greets")
+    async def hello(interaction:discord.Interaction):
+        welcome_emeb = discord.Embed(title="Hello!", description=f"Hello {interaction.user.name}!")  # noqa: E501
+        await interaction.response.send_message(embed=welcome_emeb)
+
+async def setup(bot:commands.Bot):
+    cog = Hello(bot)
+    await bot.add_cog(cog)
