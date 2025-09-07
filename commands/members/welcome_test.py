a48a9e094fa98ca93c98ead28ad480f87392adaf
xxcosita3czxx <kokosove18@gmail.com>
2025-09-08 01:05:00 +0200

    maybe shorten it and change cog name?

--- commands/members/welcome_test.py
+++ commands/members/welcome_test.py
@@ -3,15 +3,15 @@ from discord import app_commands
 from discord.ext import commands
 
 
-class Hello(commands.Cog):
+class Welcome_test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command(name="welcome-embed-test", description="Greets")
-    async def hello(interaction:discord.Interaction):
+    @app_commands.command(name="welcomeembedtest", description="Greets")
+    async def welcome_test(interaction:discord.Interaction):
         welcome_emeb = discord.Embed(title="Hello!", description=f"Hello {interaction.user.name}!")  # noqa: E501
         await interaction.response.send_message(embed=welcome_emeb)
 
 async def setup(bot:commands.Bot):
-    cog = Hello(bot)
+    cog = Welcome_test(bot)
     await bot.add_cog(cog)
